93d6c927c0799e9316e8d000e1fd44c789137c1f
Your Name <tarcham1993@gmail.com>
2019-11-01 18:43:56 +0100

    Lesson 1

---
+++ RetailDemo/ClientUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace ClientUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Main().Wait();
+            Console.WriteLine("Hello World!");
+        }
+
+        static async Task Main()
+        {
+            Console.Title = "ClientUI";
+
+            var endpointConfiguration = new EndpointConfiguration("ClientUI");
+
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
